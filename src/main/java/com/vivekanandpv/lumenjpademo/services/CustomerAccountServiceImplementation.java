f5ec4ddb6fce333235262da5cef7f4761c68b788
Vivekanand P v <vivekanandpv@outlook.com>
2024-01-05 14:32:19 +0530

    implementation of CustomerAccountServiceImplementation

--- src/main/java/com/vivekanandpv/lumenjpademo/services/CustomerAccountServiceImplementation.java
+++ src/main/java/com/vivekanandpv/lumenjpademo/services/CustomerAccountServiceImplementation.java
@@ -4,6 +4,7 @@ import com.vivekanandpv.lumenjpademo.exceptions.RecordNotFoundException;
 import com.vivekanandpv.lumenjpademo.models.Customer;
 import com.vivekanandpv.lumenjpademo.models.CustomerAccount;
 import com.vivekanandpv.lumenjpademo.repositories.CustomerAccountRepository;
+import com.vivekanandpv.lumenjpademo.repositories.CustomerRepository;
 import com.vivekanandpv.lumenjpademo.viewmodels.CustomerAccountCreateViewModel;
 import com.vivekanandpv.lumenjpademo.viewmodels.CustomerAccountViewModel;
 import com.vivekanandpv.lumenjpademo.viewmodels.CustomerViewModel;
@@ -11,43 +12,56 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerAccountServiceImplementation implements CustomerAccountService {
     private final CustomerAccountRepository customerAccountRepository;
+    private final CustomerRepository customerRepository;
 
-    public CustomerAccountServiceImplementation(CustomerAccountRepository customerAccountRepository) {
+    public CustomerAccountServiceImplementation(CustomerAccountRepository customerAccountRepository, CustomerRepository customerRepository) {
         this.customerAccountRepository = customerAccountRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public List<CustomerAccountViewModel> getAll() {
-        return null;
+        return customerAccountRepository
+                .findAll()
+                .stream()
+                .map(this::toViewModel)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerAccountViewModel getById(int id) {
-        return null;
+        return toViewModel(getEntityById(id));
     }
 
     @Override
     public List<CustomerAccountViewModel> getByCustomerId(int customerId) {
-        return null;
+        return customerAccountRepository
+                .findAll()
+                .stream()
+                .filter(a -> a.getCustomerId() == customerId)
+                .map(this::toViewModel)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerAccountViewModel create(CustomerAccountCreateViewModel viewModel) {
-        return null;
+        return toViewModel(customerAccountRepository.saveAndFlush(toEntity(viewModel)));
     }
 
     @Override
     public void deleteById(int id) {
-
+        customerAccountRepository.delete(getEntityById(id));
     }
 
     private CustomerAccountViewModel toViewModel(CustomerAccount entity) {
         CustomerAccountViewModel viewModel = new CustomerAccountViewModel();
         BeanUtils.copyProperties(entity, viewModel);
+        viewModel.setCustomer(toViewModel(entity.getCustomer()));
         return viewModel;
     }
 
@@ -58,13 +72,21 @@ public class CustomerAccountServiceImplementation implements CustomerAccountServ
     }
 
     private CustomerAccount toEntity(CustomerAccountCreateViewModel viewModel) {
+        Customer customer = getParentEntityById(viewModel.getCustomerId());
+
         CustomerAccount entity = new CustomerAccount();
         BeanUtils.copyProperties(viewModel, entity);
+        entity.setCustomer(customer);
         return entity;
     }
 
     private CustomerAccount getEntityById(int id) {
         return customerAccountRepository.findById(id)
                 .orElseThrow(() -> new RecordNotFoundException(String.format("Account with id: %d is not found", id)));
+    }
+
+    private Customer getParentEntityById(int customerId) {
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new RecordNotFoundException(String.format("Customer with id: %d is not found", customerId)));
     }
 }
